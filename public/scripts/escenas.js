2773c329dc244a642bd33fe97df662b2e87da013
Lucascn21 <lucascn21@gmail.com>
2021-02-20 11:57:26 -0300

    Eliminados assets que no usaba.
    Implementado un mapa de tiles, hecho en Tiled.
    El player ahora se puede mover sobre el mapa, aun no hay colision.

--- public/scripts/escenas.js
+++ public/scripts/escenas.js
@@ -10,7 +10,9 @@ export class Escena1 extends Phaser.Scene {
     }
 
     preload() { /*preload media for GameScene here*/
+        this.load.image("terrain", "./assets/images/terrain_atlas.png");
 
+        this.load.tilemapTiledJSON("mapa", "./assets/maps/mapa.json");
 
     }
 
@@ -28,7 +30,17 @@ export class Escena1 extends Phaser.Scene {
             this.scene.scene.start('Escena1');
 
         });
+        //Mapa
+        let mappy = this.add.tilemap("mapa");
 
+        let terrain = mappy.addTilesetImage("terrain_atlas", "terrain");
+
+        //layers
+        let baseLayer = mappy.createStaticLayer("base", [terrain], 0, 0).setDepth(-1);
+        let vegetacionLayer = mappy.createStaticLayer("vegetacion", [terrain], 0, 0);
+        let vegetacion2Layer = mappy.createStaticLayer("vegetacion2", [terrain], 0, 0);
+
+        //Player
         this.jugador = new Jugador(this.matter.world, 264, 250, 'none', 'none')
         console.dir(this.jugador)
 
